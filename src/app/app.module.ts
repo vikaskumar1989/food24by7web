204398fb5891126f3decf9754cad46fd9be5a012
Vikas Rana <vikaskumar1989@gmail.com>
2021-04-20 20:55:35 +0530

    Add firebase auth using angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyDRIIB_gVJXCDMuyc5eFfzzmOm77hAIXpg",
+  authDomain: "midnightfoodoct2018.firebaseapp.com",
+  databaseURL: "https://midnightfoodoct2018.firebaseio.com",
+  projectId: "midnightfoodoct2018",
+  storageBucket: "midnightfoodoct2018.appspot.com",
+  messagingSenderId: "435261123472",
+  appId: "1:435261123472:web:5e04ff547a800274976115"
 };
 
 /*

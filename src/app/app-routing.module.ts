816130bec04ac07c21e6ee9f4ad083b6b8abfbd0
Vikas Rana <vikaskumar1989@gmail.com>
2021-05-18 20:11:34 +0530

    Home Website Code updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
-
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AdminComponent } from './admin/admin/admin.component';
 
-const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
 const routes: Routes = [
   { path : 'login', component: LoginComponent},  
@@ -17,8 +14,7 @@ const routes: Routes = [
   { 
     path: 'home',
     component: HomeComponent,
-    canActivate: [AngularFireAuthGuard],
-    data: {authGuardPipe: redirectUnauthorizedToLogin},
+    
   },
   { 
     path : 'admin', 
